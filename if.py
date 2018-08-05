5911a6a8867bcf688216ab3b9d353f5158acc4a4
lizhaoguosina <lzgcn@outlook.com>
2018-08-05 19:11:12 +0800

    Add files via upload

--- if.py
+++ if.py
@@ -1,33 +1,33 @@
 # _*_ coding=utf-8 _*_
 
-num0 = num1 = num2 =11
-num1 = num0+1
-num2 += 1
-num3 = num0 / 3
-num4 = num0 // 3
-num5 = num0 % 2
+num0 = num1 = num2 =11 #赋值语句
+num1 = num0 + 1 #加和
+num2 += 1 # 自加
+num3 = num0 / 3 #除法
+num4 = num0 // 3 #整除
+num5 = num0 % 2 #取模
 
-print(num0,num1,num2,num3,num4)
+print(num0,num1,num2,num3,num4) #输出
 
-bol0 = num3 == num4
-bol1 = num3 != num4
-bol2 = 0
+bol0 = num3 == num4 # 布尔值为0
+bol1 = num3 != num4 # 布尔值为1
 
-if num3 == num4:
+if num3 == num4: #if语句举例
     print("相等")
 else:
     print('不相同')
 
-if bol0 == 1:
+if bol0 == 0: #布尔值判断（也可以一使用false或者true代替此处的0和1）
     print("不相同")
 else:
     print('相同')
-
+'''
 if bol1 :
     print("相同")
 else:
     print('不同')
-
+'''
+bol2 = 0
 if bol2:
     print('test')
 else:
